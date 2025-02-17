8da326984c0f1b7344b0824995909dfac7f3d0dd
Heterohabilis <xm2204@nyu.edu>
2025-02-17 16:06:33 -0500

    added types for main and test_main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from src.calculator import Calculator
 
-def main():
+def main() -> None:
     calc = Calculator()
     print(calc.add(1, 2))
 
 if __name__ == "__main__":
     main()
-
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,6 +1,7 @@
 from src.main import main
+import pytest
 
-def test_main_execution(capfd):
+def test_main_execution(capfd: pytest.CaptureFixture[str]) -> None:
     main()
     out, err = capfd.readouterr()
     assert out.strip() == "3"
