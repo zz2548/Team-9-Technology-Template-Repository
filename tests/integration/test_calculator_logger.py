63a976918a4c2743787df13d57c693db3da359de
Heterohabilis <xm2204@nyu.edu>
2025-03-06 00:07:56 -0500

    fixed PT027 and PT011 error

--- tests/integration/test_calculator_logger.py
+++ tests/integration/test_calculator_logger.py
@@ -1,6 +1,8 @@
-import unittest
 import io
+import unittest
 from unittest.mock import patch
+
+import pytest
 
 from src.calculator.calculator import Calculator
 from src.logger.logger import Logger
@@ -13,7 +15,7 @@ class TestCalculatorLogger(unittest.TestCase):
 
     def test_calculator_logger_addition(self) -> None:
         """Test Calculator operations with Logger."""
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+        with patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
             result: float = self.calculator.add(5, 3)
             self.logger.log(f"Adding 5 + 3 = {result}")
 
@@ -22,7 +24,7 @@ class TestCalculatorLogger(unittest.TestCase):
             assert "LOG: Adding 5 + 3 = 8" in captured_output, "Incorrect log output"
 
     def test_calculator_logger_subtraction(self) -> None:
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+        with patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
             result: float = self.calculator.subtract(10, 4)
             self.logger.log(f"Subtracting 10 - 4 = {result}")
 
@@ -32,7 +34,7 @@ class TestCalculatorLogger(unittest.TestCase):
                     "= 6") in captured_output, "Incorrect log output"
 
     def test_calculator_logger_multiplication(self) -> None:
-        with (patch('sys.stdout', new_callable=io.StringIO) as mock_stdout):
+        with (patch("sys.stdout", new_callable=io.StringIO) as mock_stdout):
             result: float = self.calculator.multiply(6, 7)
             self.logger.log(f"Multiplying 6 * 7 = {result}")
 
@@ -42,7 +44,7 @@ class TestCalculatorLogger(unittest.TestCase):
             captured_output, "Incorrect log output"
 
     def test_calculator_logger_division(self) -> None:
-        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+        with patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
             result: float = self.calculator.divide(10, 2)
             self.logger.log(f"Dividing 10 / 2 = {result}")
 
@@ -53,14 +55,16 @@ class TestCalculatorLogger(unittest.TestCase):
 
     def test_calculator_logger_divide_by_zero(self) -> None:
         """Ensure dividing by zero raises an error and is logged."""
-        with (patch('sys.stdout', new_callable=io.StringIO) as mock_stdout):
-            with self.assertRaises(ValueError, msg="Cannot divide by zero"):
+        with patch("sys.stdout", new_callable=io.StringIO) as mock_stdout:
+            with pytest.raises(ValueError,
+                               match = "ZeroDivisionError: division by zero"):
                 self.calculator.divide(10, 0)
             self.logger.log("Attempted to divide by zero")
 
             captured_output = mock_stdout.getvalue()
-            assert "LOG: Attempted to divide by zero" \
-            in captured_output, "Incorrect log output"
+            assert (
+                "LOG: Attempted to divide by zero" in captured_output
+            ), "Incorrect log output"
 
 
 if __name__ == "__main__":
