a332ddf00fae93f72388cf5f806e01e93125d57e
Heterohabilis <xm2204@nyu.edu>
2025-03-10 13:20:43 -0400

    add not none statements

--- src/calculator/test/test_calculator.py
+++ src/calculator/test/test_calculator.py
@@ -7,20 +7,22 @@ class TestCalculator(unittest.TestCase):
     def setUp(self) -> None:
         self.calc = Calculator()
 
-    def test_add(self)  -> None:
+    def test_return(self) -> None:
         self.assertIsNotNone(self.calc.add(2, 3))
+        self.assertIsNotNone(self.calc.subtract(6, 3))
+        self.assertIsNotNone(self.calc.multiply(2, 3))
+        self.assertIsNotNone(self.calc.divide(2, 3))
+
+    def test_add(self)  -> None:
         self.assertEqual(self.calc.add(2, 3), 5)
 
     def test_subtract(self) -> None:
-        self.assertIsNotNone(self.calc.subtract(6, 3))
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self) -> None:
-        self.assertIsNotNone(self.calc.multiply(2, 3))
         self.assertEqual(self.calc.multiply(4, 2), 8)
 
     def test_divide(self) -> None:
-        self.assertIsNotNone(self.calc.divide(2, 3))
         self.assertEqual(self.calc.divide(10, 2), 5)
 
     def test_divide_by_zero(self) -> None:
