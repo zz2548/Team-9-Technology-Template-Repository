914245ba7d64313ab624949ceb91b40ad8b30595
jinglinstella <jt4296@nyu.edu>
2025-04-27 12:40:52 -0700

    fix cicid

--- src/channel_impl/ai_bot_channel.py
+++ src/channel_impl/ai_bot_channel.py
@@ -15,14 +15,14 @@ sys.path.append(str(issue_tracker_path))
 
 
 from ai_conversation_client.providers import OpenAIClient # type: ignore[import-not-found]
-from api.src.issue_tracker import MemoryIssueTrackerClient
+from api.src.issue_tracker import MemoryIssueTrackerClient # type: ignore[import-not-found]
 
 # Create a singleton for the issue tracker
 class IssueTrackerSingleton:
-    _instance = None
+    _instance: Optional[MemoryIssueTrackerClient] = None
 
     @classmethod
-    def get_instance(cls):
+    def get_instance(cls) -> Optional[MemoryIssueTrackerClient]:
         if cls._instance is None:
             from api.src.issue_tracker import MemoryIssueTrackerClient
             try:
